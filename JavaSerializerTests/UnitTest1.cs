ac339591d6f4ecac096b950b6c1824ad9094e067
GitKepler <23261056+GitKepler@users.noreply.github.com>
2023-07-14 02:28:17 +0200

    Added test for integer arrays

--- JavaSerializerTests/UnitTest1.cs
+++ JavaSerializerTests/UnitTest1.cs
@@ -16,9 +16,11 @@ namespace JavaSerializerTests
         private readonly byte[] Sample2 = Convert.FromBase64String("rO0ABXVyAAJbSU26YCZ26rKlAgAAeHAAAAACAAAH0QAAB9I=");
         // String array with { "This", "Is", "A Test" } :: string[]
         private readonly byte[] Sample3 = Convert.FromBase64String("rO0ABXVyABNbTGphdmEubGFuZy5TdHJpbmc7rdJW5+kde0cCAAB4cAAAAAN0AARUaGlzdAACSXN0AAZBIFRlc3Q=");
+        // Integer array with { 1, 2, 3 } :: Integer[]
+        private readonly byte[] Sample4 = Convert.FromBase64String("rO0ABXVyABRbTGphdmEubGFuZy5JbnRlZ2VyO/6XraABg+IbAgAAeHAAAAADc3IAEWphdmEubGFuZy5JbnRlZ2VyEuKgpPeBhzgCAAFJAAV2YWx1ZXhyABBqYXZhLmxhbmcuTnVtYmVyhqyVHQuU4IsCAAB4cAAAAAFzcQB+AAIAAAACc3EAfgACAAAAAw==");
 
         [Test]
-        public void TestThatArraysWithSingleIntegerElementAreParsed()
+        public void TestThatArraysWithSingleIntElementAreParsed()
         {
             var memoryStream = new MemoryStream(Sample1);
             var reader = new JavaSerializer.SerializedStreamReader(memoryStream);
@@ -44,7 +46,7 @@ namespace JavaSerializerTests
         }
 
         [Test]
-        public void TestThatArraysWithMultipleIntegerElementsAreParsed()
+        public void TestThatArraysWithMultipleIntElementsAreParsed()
         {
             var memoryStream = new MemoryStream(Sample2);
             var reader = new JavaSerializer.SerializedStreamReader(memoryStream);
@@ -105,6 +107,64 @@ namespace JavaSerializerTests
                 Assert.That(typedEntries[1].String, Is.EqualTo("Is"));
                 Assert.That(typedEntries[2].String, Is.EqualTo("A Test"));
                 Assert.That(typedEntries, Has.Length.EqualTo(3));
+            });
+        }
+
+        [Test]
+        public void TestThatArraysWithMultipleIntegerElementsAreParsed()
+        {
+            var memoryStream = new MemoryStream(Sample4);
+            var reader = new JavaSerializer.SerializedStreamReader(memoryStream);
+
+            reader.Read();
+
+            Assert.That(reader.Content, Is.Not.Null);
+            Assert.That(reader.Content, Has.Count.EqualTo(1));
+
+            var content = reader.Content[0];
+
+            Assert.That(content, Is.InstanceOf<ArrayContent>());
+
+            var typedContent = (ArrayContent)content;
+            Assert.That(typedContent.Data, Is.Not.Null);
+            Assert.Multiple(() =>
+            {
+                Assert.That(typedContent.ClassDescriptor, Is.Not.Null);
+                Assert.That(typedContent.Data, Has.Length.EqualTo(3));
+                Assert.That(typedContent.Data[0], Is.InstanceOf<ObjectContent>());
+                Assert.That(typedContent.Data[1], Is.InstanceOf<ObjectContent>());
+                Assert.That(typedContent.Data[2], Is.InstanceOf<ObjectContent>());
+            });
+
+            var typedEntries = typedContent.Data.OfType<ObjectContent>().ToImmutableArray();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(typedEntries[0].Values, Is.Not.Null);
+                Assert.That(typedEntries[1].Values, Is.Not.Null);
+                Assert.That(typedEntries[2].Values, Is.Not.Null);
+                Assert.That(typedEntries, Has.Length.EqualTo(3));
+            });
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(typedEntries[0].Values?.Keys, Has.Count.EqualTo(1));
+                Assert.That(typedEntries[1].Values?.Keys, Has.Count.EqualTo(1));
+                Assert.That(typedEntries[2].Values?.Keys, Has.Count.EqualTo(1));
+            });
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(typedEntries[0].Values?.Keys.Single().Name, Is.EqualTo("value"));
+                Assert.That(typedEntries[1].Values?.Keys.Single().Name, Is.EqualTo("value"));
+                Assert.That(typedEntries[2].Values?.Keys.Single().Name, Is.EqualTo("value"));
+            });
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(typedEntries[0].Values?.Values.Single(), Is.EqualTo(1));
+                Assert.That(typedEntries[1].Values?.Values.Single(), Is.EqualTo(2));
+                Assert.That(typedEntries[2].Values?.Values.Single(), Is.EqualTo(3));
             });
         }
     }
