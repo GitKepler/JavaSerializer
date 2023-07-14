46f55fe331c3a7513e08ef5f9af7eb6719367ac5
GitKepler <23261056+GitKepler@users.noreply.github.com>
2023-07-14 14:16:59 +0200

    Added content to IClassDesciptorContent

--- JavaSerializer/Content/Object/ClassDesc/Interface/IClassDesciptorContent.cs
+++ JavaSerializer/Content/Object/ClassDesc/Interface/IClassDesciptorContent.cs
@@ -1,9 +1,12 @@
 ﻿using JavaSerializer.Content.Interface;
 using JavaSerializer.Content.Object.Inteface;
+using System.Collections.Generic;
 
 namespace JavaSerializer.Content.Object.ClassDesc.Interface
 {
     public interface IClassDesciptorContent : IObject, IClassDescriptor
     {
+        IClassDescriptor? SuperClassDescriptor { get; set; }
+        IList<IContent>? Annotations { get; set; }
     }
 }
