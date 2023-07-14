0178d2ed4cc5afbde99478ab16d96b48c50f7763
GitKepler <23261056+GitKepler@users.noreply.github.com>
2023-07-14 14:54:53 +0200

    Added field accessor to ObjectContent

--- JavaSerializer/Content/Object/ObjectContent.cs
+++ JavaSerializer/Content/Object/ObjectContent.cs
@@ -2,6 +2,7 @@
 using JavaSerializer.Content.Object.ClassDesc.FieldDescriptor.Interface;
 using JavaSerializer.Content.Object.Inteface;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace JavaSerializer.Content.Object
 {
@@ -15,5 +16,7 @@ namespace JavaSerializer.Content.Object
         public TokenType Header { get; }
         public IClassDescriptor? ClassDescriptor { get; set; }
         public IDictionary<IClassField, object>? Values { get; set; }
+
+        public IReadOnlyDictionary<string, IClassField>? Fields => Values?.Keys?.ToDictionary(key => key.Name, key => key);
     }
 }
